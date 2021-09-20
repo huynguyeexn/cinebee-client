7383a9186831d79a107586a4a1d372bc65669c4f
hui <huynguyeexn@gmail.com>
2021-09-20 12:19:46 +0700

    more data

--- components/BannerHeader.tsx
+++ components/BannerHeader.tsx
@@ -5,6 +5,25 @@ import Image from 'next/image';
 interface Props {}
 
 export const BannerHeader = (props: Props) => {
+	const bannders = [
+		{
+			imageUrl:
+				'https://www.themoviedb.org/t/p/original/lBSrAja7uIULKE5Lscl9dtdWYGA.jpg',
+		},
+		{
+			imageUrl:
+				'https://www.themoviedb.org/t/p/original/zWYCRc1Hn1gEcJtQCMvSODd6gJu.jpg',
+		},
+		{
+			imageUrl:
+				'https://www.themoviedb.org/t/p/original/lBSrAja7uIULKE5Lscl9dtdWYGA.jpg',
+		},
+		{
+			imageUrl:
+				'https://www.themoviedb.org/t/p/original/h4oh0UNGqz6CRmYVIq8ocSbKdo3.jpg',
+		},
+	];
+
 	const settings = {
 		className: 'center',
 		accessibility: true,
@@ -22,26 +41,21 @@ export const BannerHeader = (props: Props) => {
 	return (
 		<div className="banner-header">
 			<Slider {...settings}>
-				<div className="">
-					<div className="banner-header-item">
-						<img src="https://www.themoviedb.org/t/p/original/lBSrAja7uIULKE5Lscl9dtdWYGA.jpg" />
-					</div>
-				</div>
-				<div className="">
-					<div className="banner-header-item">
-						<img src="https://www.themoviedb.org/t/p/original/9xY4WB553gGdkt3SAgEj75MpRfa.jpg" />
-					</div>
-				</div>
-				<div className="">
-					<div className="banner-header-item">
-						<img src="https://www.themoviedb.org/t/p/original/zWYCRc1Hn1gEcJtQCMvSODd6gJu.jpg" />
-					</div>
-				</div>
-				<div className="">
-					<div className="banner-header-item">
-						<img src="https://www.themoviedb.org/t/p/original/h4oh0UNGqz6CRmYVIq8ocSbKdo3.jpg" />
-					</div>
-				</div>
+				{bannders &&
+					bannders.map((banner, idx) => (
+						<div className="" key={idx}>
+							<div className="banner-header-item">
+								<Image
+									priority={true}
+									src={banner.imageUrl}
+									loader={() => banner.imageUrl}
+									alt=""
+									layout="fill"
+									objectFit="cover"
+								/>
+							</div>
+						</div>
+					))}
 			</Slider>
 		</div>
 	);
