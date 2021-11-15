1b20ebd625ca9e747e96a4c8ee8cc5ab2349e59a
hui <huynguyeexn@gmail.com>
2021-11-15 19:04:17 +0700

    Playing, Coming Movies API

--- api-client/movieApi.ts
+++ api-client/movieApi.ts
@@ -4,23 +4,31 @@ import axiosClient from './axiosClient';
 const endpoint = '/movies';
 
 export const movieApi = {
-	getById: (id: string | string[] ) => {
+	getById: (id: string | string[]) => {
 		return axiosClient.get(endpoint + `/${id}`);
 	},
 
-    getMovieByGenres: (id: number) => {
+	getMovieByGenres: (id: number) => {
 		return axiosClient.get(endpoint + `/${id}/genres`);
 	},
 
-    getMovieByDirectors: (id: number) => {
+	getMovieByDirectors: (id: number) => {
 		return axiosClient.get(endpoint + `/${id}/directors`);
 	},
 
-    getMovieByActors: (id: number) => {
+	getMovieByActors: (id: number) => {
 		return axiosClient.get(endpoint + `/${id}/actors`);
 	},
 
-    getMovie: () => {
+	getMovie: () => {
 		return axiosClient.get(endpoint);
+	},
+
+	getMoviesComing: (limit: number = 10) => {
+		return axiosClient.get(endpoint + `/?filter=2&filter_by=status&per_page=${limit}`);
+	},
+
+	getMoviesPlaying: (limit: number = 10) => {
+		return axiosClient.get(endpoint + `/?filter=1&filter_by=status&per_page=${limit}`);
 	},
 };
